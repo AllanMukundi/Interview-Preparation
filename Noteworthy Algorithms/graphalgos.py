1679fa9a40932f9177a8ab61794a7d16ab5aa388
Allan M <allanmukundi@hotmail.com>
2017-07-18 10:12:45 -0400

    Dijkstra's added - graphalgos.py

--- Noteworthy Algorithms/graphalgos.py
+++ Noteworthy Algorithms/graphalgos.py
@@ -1,5 +1,6 @@
 from graph import Graph
 from collections import deque
+from heappriorityqueue import HeapPriorityQueue
 
 def dfs(graph, start):
     """
@@ -49,6 +50,62 @@ def path(graph, start, goal):
         path.reverse()
     return path
 
+def topological_sort(graph):
+    """
+    Returns a list of Vertices of the specified Directed Acyclic Graph in topological order.
+    Note: if the specified Graph has a cycle, the result will be incomplete.
+    """
+    topo = []
+    ready = []    # Vertices with no remaining constraints
+    incount = {}    # tracks indeg(v) for each Vertex
+    for each in graph.vertices():
+        incount[each] = graph.degree(each, False)    # specifies indeg(v) instead of outdeg(v)
+        if (incount[each] == 0):
+            ready.append(each)
+    while len(ready) > 0:
+        vertex = ready.pop()
+        topo.append(vertex)
+        for edge in graph.incident_edges(vertex):
+            opposite = edge.opposite(vertex)
+            incount[opposite] -= 1
+            if (incount[opposite] == 0):
+                ready.append(opposite)
+    return [x.element() for x in topo]
+    return topo 
+
+def djikstra(graph, start):
+    """
+    Computes the shortest-path distances from the Vertex start to all Vertices of the given Graph.
+
+    The given Graph can be undirected or directed, but must be weighted such that edge.element()
+    returns a non-negative weight for each edge e.
+
+    Returns a dictionary mapping every reachable Vertex to its distance from start.
+    """
+    d = {}
+    cloud = {}
+    pq = HeapPriorityQueue()
+    locate = {}
+    for vertex in graph.vertices():
+        if vertex is start:
+            d[vertex] = 0
+        else:
+            d[vertex] = float('inf')
+        locate[vertex] = pq.add(d[vertex], vertex)
+    while not pq.is_empty():
+        weight, vertex = pq.remove_min()
+        cloud[vertex] = weight
+        del locate[vertex]
+        for edge in graph.incident_edges(vertex):
+            opposite = edge.opposite(vertex)
+            if opposite not in cloud:
+                cur_weight = edge.element()
+                if (d[vertex] + cur_weight < d[opposite]):
+                    d[opposite] = d[vertex] + cur_weight
+                    pq.update(locate[opposite], d[opposite], opposite)
+    return cloud
+
+
 if __name__ == '__main__':
     airports = Graph(True)
     SFO = airports.add_vertex('SFO')
@@ -63,7 +120,6 @@ if __name__ == '__main__':
     airports.add_edge(DFW, SFO, 3)
     airports.add_edge(JFK, BOS, 4)
     airports.add_edge(BOS, JFK, 5)
-    airports.add_edge(DFW, JFK, 5)
     airports.add_edge(MIA, LAX, 6)
     airports.add_edge(DFW, LAX, 7)
     airports.add_edge(LAX, ORD, 8)
@@ -74,4 +130,26 @@ if __name__ == '__main__':
     airports.add_edge(JFK, MIA, 13)
     airports.add_edge(ORD, MIA, 14)
     airports.add_edge(BOS, MIA, 15)
+    dag = Graph(True)
+    A = dag.add_vertex('A')
+    B = dag.add_vertex('B')
+    C = dag.add_vertex('C')
+    D = dag.add_vertex('D')
+    E = dag.add_vertex('E')
+    F = dag.add_vertex('F')
+    G = dag.add_vertex('G')
+    H = dag.add_vertex('H')
+    dag.add_edge(A, C)
+    dag.add_edge(A, D)
+    dag.add_edge(B, D)
+    dag.add_edge(B, F)
+    dag.add_edge(C, D)
+    dag.add_edge(D, F)
+    dag.add_edge(C, E)
+    dag.add_edge(E, G)
+    dag.add_edge(F, G)
+    dag.add_edge(G, H)
+    dag.add_edge(F, H)
+    dag.add_edge(C, H)
+    print(djikstra(airports, MIA))
 
